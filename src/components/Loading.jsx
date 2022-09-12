d5afc79429b706d4054f80b9d7155968f643f9e5
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-09-12 09:59:38 +0600

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
-
 export const Loading = () => (
   <div className="flex justify-center items-center ">
     <Loader type="Puff" color="#00BFFF" height={550} width={80} />
